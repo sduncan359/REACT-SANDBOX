760c5834e4a9f45e2c0eff5386f1bb6a519d39f6
Steve Duncan <67704665+sduncan-propack@users.noreply.github.com>
2023-01-25 18:46:34 -0800

    more hooks

---
+++ src/components/UseCallbackExample.jsx
@@ -0,0 +1,25 @@
+import React, { useCallback, useState } from "react"
+
+export default function UseCallbackExample() {
+    const [tasks, setTasks] = useState([])
+
+    const addTask = useCallback(() => {
+        setTasks((prevState) => [...prevState, 'Some Task'])
+    }, [setTasks])
+
+    return (
+        <div>
+            <Button addTask={addTask} />
+            {tasks.map((task, index) => (
+                <p key={index}>{task}</p>
+    ))}
+        </div>
+    )
+}
+
+const Button = React.memo(({addTask}) => {
+    console.log('Button rendered')
+    return <div>
+        <button className="btn btn-primary" onClick={addTask}>Add Task</button>
+    </div>
+})
